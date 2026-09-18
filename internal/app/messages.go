92f746c060f974e17da591d0072ac4c1daee0215
zazga <s.vandenhurk@gmail.com>
2026-09-18 12:18:40 +0200

    Add typed Bubble Tea messages

---
+++ internal/app/messages.go
@@ -0,0 +1,14 @@
+package app
+
+import (
+	"github.com/mattermost/mattermost/server/public/model"
+	mm "github.com/zazga/kmo/internal/mattermost"
+)
+
+type bootstrapMsg struct { session *mm.Session; err error }
+type setupValidatedMsg struct { service *mm.Service; session *mm.Session; url string; err error }
+type messagesLoadedMsg struct { channelID string; posts []*model.Post; users map[string]*model.User; hasOlder bool; page int; err error }
+type postSentMsg struct { post *model.Post; err error }
+type conversationRefreshMsg struct { items []*mm.Conversation; err error }
+type websocketMsg struct { event mm.Event }
+type markedReadMsg struct { channelID string; err error }
