d537a911b69121f280a2e886da21708ce706b8f6
zazga <s.vandenhurk@gmail.com>
2026-09-19 09:42:59 +0200

    Add daemon status message

--- internal/app/messages.go
+++ internal/app/messages.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"github.com/mattermost/mattermost/server/public/model"
+	"github.com/zazga/kmo/internal/daemon"
 	mm "github.com/zazga/kmo/internal/mattermost"
 )
 
@@ -12,3 +13,4 @@ type postSentMsg struct { post *model.Post; err error }
 type conversationRefreshMsg struct { items []*mm.Conversation; err error }
 type websocketMsg struct { event mm.Event }
 type markedReadMsg struct { channelID string; err error }
+type daemonStatusMsg struct { status daemon.Status }
